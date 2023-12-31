5a4fe51d60931e3bf7138c62502969905623ebd9
yoswos <151448407+yoswosyotelc@users.noreply.github.com>
2023-12-31 17:36:59 +0300

    Add files via upload

---
+++ Sololearn.java
@@ -0,0 +1,18 @@
+// @#$!@#$@!#$Progress### << ///Dec_2023..??>> @ @#$@%#!@%!Saving??@!#$@!#$@!#$!@#$@!#$
+// !!!!! don't stay at one part for more than 1 weekds...!!!!
+// !!!!! proceed as you go through by commenting .. for furhter refirinement..
+// //..//...//..//...//..//..//..//...//
+
+class HelloWorld {
+    static void main(String[] args) {
+        // Print 'Hello, World!' to the console
+        System.out.println("Hello, World!");
+    }
+}
+/*
+you need to install netbeans for these is that 
+what you are seaying to me man?? or ecclipse... please choose 
+the bet IDE..
+*/
+
+
